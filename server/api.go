1afa025f9724ffe868cc67caca6c413b3ba268ed
Lee Woodridge <le3wood@gmail.com>
2016-07-21 15:48:08 -0700

    Changed search endpoint to work on json encoded request body, and to reply in json too.

--- server/api.go
+++ server/api.go
@@ -22,7 +22,7 @@ func StartServer(cards prep.SearchInfo) {
 	}
 
 	http.HandleFunc("/card/", getCard(cards))
-	http.HandleFunc("/search/", search(cards))
+	http.HandleFunc("/search", search(cards))
 	fmt.Printf("Now serving on port: %s\n", port)
 	http.ListenAndServe(":"+port, nil)
 }
@@ -43,14 +43,24 @@ func getCard(cards prep.SearchInfo) http.HandlerFunc {
 	}
 }
 
-// search is the function which handles the /search/ endpoint.
-// It returns a list of cards which match the search term.
+// search is the function which handles the /search endpoint.
+// Queries are json encoded, and assumed to have the form:
+// {
+// 		"search": "query string"
+// }
+// It returns a list of cards which match the search term, json encoded.
 func search(cards prep.SearchInfo) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		searchTerm := strings.SplitN(r.URL.Path, "/", 3)[2]
+		var query struct {
+			Search string
+		}
+		if err := json.NewDecoder(r.Body).Decode(&query); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
 
-		res := cards.Trie.FuzzySearch(searchTerm, 1)
+		res := cards.Trie.FuzzySearch(query.Search, 1)
 
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		json.NewEncoder(w).Encode(res)
 	}
 }
